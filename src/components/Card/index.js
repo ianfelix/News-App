3aa338dd95ea8d9ec9a7691b29c0da8b1f0c63a1
Ian Felix <62842327+ianfelix@users.noreply.github.com>
2020-10-04 12:21:20 -0300

    remove state from Card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import HeaderCard from './HeaderCard';
 import ImgCard from './ImgCard';
 import FooterCard from './FooterCard';
